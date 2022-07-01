5d8a756c736e1287dd673064e85ac238f80e9b9b
millierichards <106369169+millierichards@users.noreply.github.com>
2022-07-01 11:34:01 +0300

    Add files via upload

---
+++ STRUCTURED PROGRAMMING/simple calculator/main.c
@@ -0,0 +1,34 @@
+/*
+simple calculator
+created by Millicent sang dit-01-0072/2022
+on July, 2022
+c89 complier
+MIT license
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1,num2,sum,diff,product;
+    float quotient;
+    printf("\tsimple calculator!\n");
+    printf("Enter two numbers: ");
+    scanf("%d%d",&num1,&num2);
+
+    //computations
+    sum = num1 + num2;
+    diff = num1 - num2;
+    product = num1 * num2;
+    quotient =(float)num1/num2;
+
+    //output
+    printf("%d+%d=%d\n",num1,num2,sum);
+    printf("%d-%d=%d\n",num1,num2,diff);
+    printf("d%*d%=%d\n",num1,num2,product);
+    printf("%d/%d=%f\n",num1,num2,quotient);
+
+
+    return 0;
+}
